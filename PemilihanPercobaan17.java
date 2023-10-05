6c9cb9304200b836133cd6eaf6f17bdf8b1bbbd8
M-AdityoRahman <144311783+M-AdityoRahman@users.noreply.github.com>
2023-10-05 12:08:52 +0700

    Add files via upload

---
+++ PemilihanPercobaan17.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PemilihanPercobaan17 {
+    public static void main(String[] args) {
+        Scanner input17 = new Scanner (System.in);
+        System.out.println("Masukkan angka: ");
+        int angka = input17.nextInt();
+        if (angka % 2 == 0) {
+            System.out.println("Angka " +angka+ " bilangan genap");
+        }
+        else{}
+        System.out.println("Angka " +angka+ " bilangan ganjil");
+    }
+}
