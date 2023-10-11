52961d9e80cfd927ed49b302c519e41f2f2c33a8
M-AdityoRahman <144311783+M-AdityoRahman@users.noreply.github.com>
2023-10-11 20:33:56 +0700

    Update Pemilihan2percobaan117.java

--- Pemilihan2percobaan117.java
+++ Pemilihan2percobaan117.java
@@ -6,8 +6,7 @@ public class Pemilihan2percobaan117 {
             System.out.println("Masukkan tahun : ");
             tahun = input17.nextInt();
 
-            if ((tahun % 4) == 0) {
-                if ((tahun % 100) != 0)
+            if ((tahun % 4 == 0 && tahun % 100 !=0)) {
                  System.out.println(" Tahun Kabisat");
                     
                 }else
