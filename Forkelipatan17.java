afbb73b2635aff55124a67c3b2cf0d4bdce80b92
M-AdityoRahman <144311783+M-AdityoRahman@users.noreply.github.com>
2023-10-16 14:30:42 +0700

    Update Forkelipatan17.java

--- Forkelipatan17.java
+++ Forkelipatan17.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 public class Forkelipatan17 {
     public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
-        int kelipatan, total, counter;
-        total = 0 ;
-        counter = 0 ; 
+        int kelipatan, total=0, counter=0;
+        double rataRata; 
         System.out.println( "Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
         for (   int i = 1 ; i <= 50; i++) {
@@ -14,7 +13,9 @@ public class Forkelipatan17 {
             }
             
         }
+        rataRata = (double) total / counter ;
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Rata-rata kelipatan bilangan tersebut adalah " + rataRata);
     }
 }
