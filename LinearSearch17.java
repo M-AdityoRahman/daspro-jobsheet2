40ac77bfacef1ecb4a9ab93778305fec6b93a415
M-AdityoRahman <144311783+M-AdityoRahman@users.noreply.github.com>
2023-10-26 14:52:44 +0700

    Add files via upload

---
+++ LinearSearch17.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class LinearSearch17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int key = 0;
+        int hasil = 0, jml, status = 1;
+        System.out.print("Masukkan jumlah elemen array : ");
+        jml = input.nextInt();
+        int [] arrayInt = new int [jml];
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + " : ");
+            arrayInt [i] = input.nextInt();
+        }
+        System.out.print("Masukkan key yang ingin dicari : ");
+        key = input.nextInt();
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt [i] == key && key==0) {
+                System.out.println("Masukkan key yang ingin dicari : 0");
+                status = -1;
+                System.exit(status);
+            }else if (arrayInt [i] == key && key !=0) {
+                hasil += i ;
+                break;
+            }
+        }
+        if (hasil==0) {
+        System.out.println("Key tidak ditemukan");
+        }else if (hasil != 0) {
+            System.out.println("Key ada di posisi indeks ke- " + hasil);
+        }
+    }
+}
